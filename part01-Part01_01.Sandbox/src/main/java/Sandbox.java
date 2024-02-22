c9825cf13e8a982036a7a1c05c5f81a615de2ae3
DeanDaDon23 <114552500+DeanDaDon23@users.noreply.github.com>
2024-02-21 17:31:44 -0800

    Add files via upload

---
+++ part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -0,0 +1,9 @@
+
+public class Sandbox {
+
+    public static void main(String[] args) {
+        // Write your program here
+        System.out.println("Hello World");
+        System.out.println("This is a test");
+    }
+}
